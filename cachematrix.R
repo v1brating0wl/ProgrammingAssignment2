649634595405134c172d1cd12ecc1a09b5f39284
v1brating0wl <carly.buckner@gmail.com>
2015-03-12 20:38:15 +0100

    Add headings and initial comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+#-----------------------------------------------------------------------------------#
+#                                 Cache Inverse Matrix                              #
+#                                                                                   #
+# Programmed by:  Caroline Buckner                                                  #
+# Programmed for: R Programming - Coursera                                          #
+# Date:           12 March 2015                                                     #
+# Date modified:  -- March 2015                                                     #
+#-----------------------------------------------------------------------------------#
+# Description:                                                                      #
+#     The function takes in a square matrix. The matrix is converted into a         #
+#     cache-able object. The matrix is inverted. Result is cached.                  #
+#-----------------------------------------------------------------------------------#
 
-## Write a short comment describing this function
+
+#-----------------------------------------------------------------------------------#
+#  The function creates a special "matrix" object that can cache its inverse        #
+#-----------------------------------------------------------------------------------#
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+#-----------------------------------------------------------------------------------#
+# The  function computes the inverse of the object returned by makeCacheMatrix.     #
+#  If the matrix is unchanged and the inverse has already been calculated,          #
+#  then the function retrieves the inverse from the cache.                          #
+#-----------------------------------------------------------------------------------#
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
